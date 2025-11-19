c543cde0ae9cb6d64a86c6a13da2960cbc31119f
xnemcovmend <eliskanemcova1122@seznam.cz>
2025-11-19 23:19:34 +0100

    Created Item.cpp and Item .h with basic functionality

--- gameska/frontEnd/Classes/Item.cpp
+++ gameska/frontEnd/Classes/Item.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "item.h"
+
+Item::Item(string name, string type, int bonusPower)
+    : m_name(name), m_type(type), m_bonusPower(bonusPower){
+}
+
+
+string Item::getName() const{
+return m_name;
+}
+
+string Item::getType() const{
+return m_type;
+}
+
+int Item::getBonusPower() const{
+return m_bonusPower;
+}
--- gameska/frontEnd/Classes/Item.h
+++ gameska/frontEnd/Classes/Item.h
@@ -5,8 +5,21 @@
 #ifndef ZOO_ZS2025_2026_XJANOTA3_ITEM_H
 #define ZOO_ZS2025_2026_XJANOTA3_ITEM_H
 
+using namespace std;
 
 class item {
+private:
+    string m_name;    //Dagger, sword, bow etc.
+    string m_type;   //Weapon, key, scroll,
+    int m_bonusPower; // +1 dagger / +2 sword /+0key
+
+public:
+item( string name, string type, int bonusPower);
+
+string getName() const;
+string getType() const;
+int getBonusPower() const;
+
 };
 
 
