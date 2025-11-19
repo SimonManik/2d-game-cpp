a8b366b0eb880739482d2ef0e16cf266a5a0180e
xnemcovmend <eliskanemcova1122@seznam.cz>
2025-11-19 22:18:06 +0100

    Initial commit

---
+++ gameska/frontEnd/Classes/Enemy.h
@@ -0,0 +1,13 @@
+//
+// Created by elisk on 19.11.2025.
+//
+
+#ifndef ZOO_ZS2025_2026_XJANOTA3_ENEMY_H
+#define ZOO_ZS2025_2026_XJANOTA3_ENEMY_H
+
+
+class enemy {
+};
+
+
+#endif //ZOO_ZS2025_2026_XJANOTA3_ENEMY_H
