b703e182b4a9f1947a48a88c72c7a635839166b8
Šimon-maník <xmanik@mendelu.cz>
2025-11-23 14:10:47 +0100

    vytvoreni tridy mapy

---
+++ gameska/frontEnd/Classes/map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by manik on 23.11.2025.
+//
+
+#include "map.h"
---
+++ gameska/frontEnd/Classes/map.h
@@ -0,0 +1,13 @@
+//
+// Created by manik on 23.11.2025.
+//
+
+#ifndef GAMESKA_MAP_H
+#define GAMESKA_MAP_H
+
+
+class map {
+};
+
+
+#endif //GAMESKA_MAP_H
